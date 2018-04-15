c28ba1bbbc21473bc0ed129ab6cfdf71a2be86c6
Anastat <anastat@metropolia.fi>
2018-04-15 15:48:28 +0300

    dialog for setting deck name

--- app/src/main/java/com/project/studycards/MainActivity.java
+++ app/src/main/java/com/project/studycards/MainActivity.java
@@ -1,5 +1,7 @@
 package com.project.studycards;
 
+import android.app.DialogFragment;
+import android.content.Intent;
 import android.support.constraint.ConstraintLayout;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
@@ -10,11 +12,13 @@ import android.widget.GridLayout;
 import android.widget.TableLayout;
 
 import com.project.studycards.model.Deck;
+import com.project.studycards.model.DeckNameDialogFragment;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements DeckNameDialogFragment.DeckNameDialogListener{
 
     private FloatingActionButton btnAddNewDeck;
     private TableLayout mainTableLayout;
+    private String deckName;
     //private GridLayout mainGridLayout;
     private View.OnClickListener addNewDeckOnClickistener = new View.OnClickListener() {
         @Override
@@ -23,6 +27,12 @@ public class MainActivity extends AppCompatActivity {
         }
     };
 
+    private View.OnClickListener openDeckModes = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            openDeckMenu();
+        }
+    };
 
 
     @Override
@@ -37,10 +47,27 @@ public class MainActivity extends AppCompatActivity {
 
     //create new button on main screen with deck name
     private void addNewDeckBtnClicked() {
+        deckName = "";
+        //Deck newDeck = new Deck("New deck");
+        DialogFragment deckNameDialog = new DeckNameDialogFragment();
+        deckNameDialog.show(getFragmentManager(), "missiles");
+    }
 
-        Deck newDeck = new Deck("New deck");
-        Button btnNew = new Button(this);
-        btnNew.setText("New deck");
-        mainTableLayout.addView(btnNew);
+    //open new screen with deck modes
+    private void openDeckMenu () {
+        Intent intent = new Intent(this, DeckModes.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void applyDeckname(String deckName) {
+        this.deckName = deckName;
+        if (!deckName.isEmpty()) {
+            //create new button with deck name
+            Button btnNew = new Button(this);
+            btnNew.setText(deckName);
+            btnNew.setOnClickListener(openDeckModes);
+            mainTableLayout.addView(btnNew);
+        }
     }
 }
---
+++ app/src/main/java/com/project/studycards/model/DeckNameDialogFragment.java
@@ -0,0 +1,60 @@
+package com.project.studycards.model;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.app.DialogFragment;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.widget.EditText;
+
+import com.project.studycards.MainActivity;
+import com.project.studycards.R;
+//dialog screen for typing deckname
+public class DeckNameDialogFragment extends DialogFragment {
+
+    private DeckNameDialogListener listener;
+
+
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+        // Use the Builder class for convenient dialog construction
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+        final EditText input = new EditText(getActivity());
+
+        builder.setMessage(R.string.typeDeckName)
+                .setView(input)
+                // Add action buttons
+                .setPositiveButton(R.string.save, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // save deck name
+                        listener.applyDeckname(input.getText().toString());
+
+                    }
+                })
+                .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        // User cancelled the dialog
+                        DeckNameDialogFragment.this.getDialog().cancel();
+                    }
+                });
+        // Create the AlertDialog object and return it
+        return builder.create();
+    }
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        try {
+            listener = (DeckNameDialogListener) context;
+        } catch (ClassCastException e) {
+            throw new ClassCastException(context.toString() + "Must implement deckNameDialogListener");
+        }
+    }
+
+    public interface DeckNameDialogListener {
+        void applyDeckname(String deckName);
+    }
+}
