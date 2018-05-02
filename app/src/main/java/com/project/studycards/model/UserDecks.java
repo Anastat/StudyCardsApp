0d4347c6afb4000e9f72341b1a41c579320af4d0
Johanna Lamppu <johanna.e.lamppu@gmail.com>
2018-05-02 15:39:10 +0300

    A method for writing decks to files created, incomplete

--- app/src/main/java/com/project/studycards/model/UserDecks.java
+++ app/src/main/java/com/project/studycards/model/UserDecks.java
@@ -6,8 +6,12 @@ import org.supercsv.cellprocessor.ParseInt;
 import org.supercsv.cellprocessor.constraint.NotNull;
 import org.supercsv.cellprocessor.ift.CellProcessor;
 import org.supercsv.io.CsvBeanReader;
+import org.supercsv.io.CsvBeanWriter;
 import org.supercsv.io.ICsvBeanReader;
+import org.supercsv.io.ICsvBeanWriter;
 import org.supercsv.prefs.CsvPreference;
+
+import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
@@ -72,4 +76,67 @@ public class UserDecks {
     public void add(Deck newDeck) {
         this.decks.add(newDeck);
     }
+
+    public void writeDecksToFiles(AssetManager assetManager) {
+        this.assetManager = assetManager;
+
+        try {
+            String[] filelist = assetManager.list("decks");
+            if (filelist == null) {
+                // dir does not exist or is not a directory
+            } else {
+
+                for (Deck deck : decks) {
+                    //find out if file already exists
+                    String newFileName = deck.getName() + ".csv";
+                    boolean fileExists = false;
+
+                    for (int i=0; i<filelist.length; i++) {
+                        String filename = filelist[i];
+                        if (newFileName.equals(filename)) {
+                            fileExists = true;
+                        }
+                    }
+
+                    if (fileExists) {
+                        //if filename exists, go through cards, save new ones
+
+                    } else {
+                        //save deck and its cards
+                        ICsvBeanWriter beanWriter = null;
+                        try {
+                            beanWriter = new CsvBeanWriter(new FileWriter("decks/" + newFileName),
+                                    CsvPreference.STANDARD_PREFERENCE);
+
+                            // the header elements are used to map the bean values to each column (names must match)
+                            final String[] header = new String[] {"question", "answer", "priority", "count"};
+                            final CellProcessor[] processors = new CellProcessor[]{new NotNull(),new NotNull(), new ParseInt(), new ParseInt()};
+
+                            // write the header
+                            beanWriter.writeHeader(header);
+
+                            // write the beans
+                            for (final Card card : deck.getCards()) {
+                                beanWriter.write(card, header, processors);
+                            }
+
+                        } finally {
+                            if(beanWriter != null) {
+                                beanWriter.close();
+                            }
+                        }
+                    }
+
+                }
+
+
+            }
+
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
 }
